4841e3b213f26acef6a0b76b0dca5e296f87fe3b
Umberto <9914422+unoe@users.noreply.github.com>
2022-11-11 10:03:13 +0000

    set seed

--- internal_data/sem1/s1w9_binom_data.R
+++ internal_data/sem1/s1w9_binom_data.R
@@ -1,6 +1,8 @@
 # https://today.yougov.com/topics/society/articles-reports/2022/09/29/how-do-people-us-take-their-coffee
 # P(coffee) = 3/4 = 0.75
 
+set.seed(1234)
+
 library(tidyverse)  # we use read_csv and glimpse from tidyverse
 tips <- read_csv("https://uoepsy.github.io/data/RestaurantTips.csv")
 head(tips)
@@ -13,3 +15,5 @@ tips2 <- tips %>%
 
 N = nrow(tips2)
 tips2$HadCoffee <- rbinom(n = N, size = 2, prob = 0.75)
+
+write_csv(tips2, "RestaurantTips2.csv")
