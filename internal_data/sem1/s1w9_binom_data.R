7b55e74bc96e34c399fa9116a14625f132fb36ee
Umberto <9914422+unoe@users.noreply.github.com>
2022-11-11 09:55:18 +0000

    binom data

---
+++ internal_data/sem1/s1w9_binom_data.R
@@ -0,0 +1,15 @@
+# https://today.yougov.com/topics/society/articles-reports/2022/09/29/how-do-people-us-take-their-coffee
+# P(coffee) = 3/4 = 0.75
+
+library(tidyverse)  # we use read_csv and glimpse from tidyverse
+tips <- read_csv("https://uoepsy.github.io/data/RestaurantTips.csv")
+head(tips)
+
+table(tips$Guests)
+
+# keep only parties of 2 people
+tips2 <- tips %>%
+    filter(Guests == 2)
+
+N = nrow(tips2)
+tips2$HadCoffee <- rbinom(n = N, size = 2, prob = 0.75)
