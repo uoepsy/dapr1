de65ea8546dc21b97b644d7d1a1eb6e0abceec43
Josiah King <josiahpjking@gmail.com>
2026-08-07 09:30:33 +0100

    skill tab + survey

--- dapr1_workshops/survey_sort.R
+++ dapr1_workshops/survey_sort.R
@@ -1,27 +1,15 @@
-df <-readxl::read_xlsx("DAPR1 Welcome Survey!(1-3).xlsx")[,7:19]
+library(qualtRics)
+library(tidyverse)
 
-names(df) <- c(
-  "pseudonym",
-  "birthmonth",
-  "am_pm",
-  "n_sibling",
-  "spicy",
-  "eye_colour",
-  "distance_born",
-  "sleepqual",
-  "procrast",
-  "multitask",
-  "fav_colour",
-  "catdog",
-  "threewords"
-)
+df <- fetch_survey("SV_7VxqkFo12wBita6") |> 
+  janitor::clean_names() |>
+  filter(status != "Survey Preview")
 
-df <- df |> dplyr::transmute(
-  pseudonym = tolower(pseudonym),
-  birthmonth = tolower(birthmonth),
-  am_pm = tolower(am_pm),
-  n_sibling = as.numeric(n_sibling),
-  spicy = tolower(sub(":.*","",spicy)),
+outdf <- tibble(
+  pseudonym = tolower(df$q2),
+  birthmonth = tolower(df$q3),
+  n_sibling = as.numeric(df$q4),
+  spicy = tolower(sub(":.*","",df$q7)),
   spicy_num = dplyr::case_when(
     spicy == "anti-spice" ~ 1,
     spicy == "mild" ~ 2,
@@ -31,14 +19,16 @@ df <- df |> dplyr::transmute(
     spicy == "extreme" ~ 6,
     TRUE ~ NA
   ),
-  eye_colour = tolower(eye_colour),
-  distance_born = as.numeric(distance_born),
-  sleepqual = as.numeric(sleepqual),
-  procrast = as.numeric(procrast),
-  multitask = as.numeric(multitask),
-  fav_color = tolower(fav_colour),
-  catdog = tolower(catdog),
-  threewords = tolower(threewords)
+  eye_colour = tolower(df$q8),
+  distance_born = as.numeric(df$q9),
+  outlook = as.numeric(df$q10_1),
+  ampm = as.numeric(df$q11_1),
+  sleepqual = as.numeric(df$q12_1),
+  procrast = as.numeric(df$q13_1),
+  multitask = as.numeric(df$q14_1),
+  fav_color = tolower(df$q15),
+  catdog = tolower(df$q16),
+  threewords = tolower(df$q17)
 )
 
 readr::write_csv(df,file="../../data/dapr1_2526_survey.csv")
